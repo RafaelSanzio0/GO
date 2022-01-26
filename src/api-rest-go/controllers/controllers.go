bba1d0812da460f39e91765937cf5bc853b3d915
Rafael Sanzio <rafaelsanzio16@gmail.com>
2022-01-26 08:35:09 -0300

    finalizando curso de api rest com go

--- src/api-rest-go/controllers/controllers.go
+++ src/api-rest-go/controllers/controllers.go
@@ -14,11 +14,10 @@ func Home(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprint(w, "Home Page")
 }
 
+// CRUD
 func AllPersonalidades(w http.ResponseWriter, r *http.Request) {
 	var p []models.Personalidade
-
-	database.DB.Find(&p) // encontra no banco a personalidade
-
+	database.DB.Find(&p)         // encontra no banco a personalidade
 	json.NewEncoder(w).Encode(p) // quero ter um novo encoder do tipo JSON e encondar todas as minhas personalidades
 }
 
@@ -26,8 +25,31 @@ func PersonalidadesById(w http.ResponseWriter, r *http.Request) {
 	vars := mux.Vars(r)
 	id := vars["id"]
 	var p models.Personalidade
-
 	database.DB.First(&p, id)
-
 	json.NewEncoder(w).Encode(p)
+}
+
+func CriaUmaNovaPersonalidade(w http.ResponseWriter, r *http.Request) {
+	var novaPersonalidade models.Personalidade
+	json.NewDecoder(r.Body).Decode(&novaPersonalidade) // PEGA A INFO QUE ESTA EM FORMATO JSON E MUDA PARA UM OUTRO FORMATO PARA SALVAR NO BD
+	database.DB.Create(&novaPersonalidade)
+	json.NewEncoder(w).Encode(novaPersonalidade)
+}
+
+func DeletaUmaPersonalidade(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	id := vars["id"]
+	var personalidade models.Personalidade
+	database.DB.Delete(&personalidade, id)
+	json.NewEncoder(w).Encode(personalidade)
+}
+
+func EditaPersonalidade(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	id := vars["id"]
+	var personalidade models.Personalidade
+	database.DB.First(&personalidade, id)
+	json.NewDecoder(r.Body).Decode(&personalidade)
+	database.DB.Save(&personalidade)
+	json.NewEncoder(w).Encode(personalidade)
 }
--- src/api-rest-go/routes/routes.go
+++ src/api-rest-go/routes/routes.go
@@ -5,15 +5,20 @@ import (
 	"net/http"
 
 	"github.com/RafaelSanzio0/GO/go-rest-api/controllers"
+	"github.com/RafaelSanzio0/GO/go-rest-api/middleware"
 	"github.com/gorilla/mux"
 )
 
 func HandleRequest() {
 	r := mux.NewRouter()
+	r.Use(middleware.ContentTypeMiddleware)
 
 	r.HandleFunc("/", controllers.Home) // quando cair no endereço barra vazio joga pra função home atender
-	r.HandleFunc("/api/personalidades", controllers.AllPersonalidades)
-	r.HandleFunc("/api/personalidades/{id}", controllers.PersonalidadesById)
+	r.HandleFunc("/api/personalidades", controllers.AllPersonalidades).Methods("Get")
+	r.HandleFunc("/api/personalidades/{id}", controllers.PersonalidadesById).Methods("Get")
+	r.HandleFunc("/api/personalidades", controllers.CriaUmaNovaPersonalidade).Methods("Post")
+	r.HandleFunc("/api/personalidades/{id}", controllers.DeletaUmaPersonalidade).Methods("Delete")
+	r.HandleFunc("/api/personalidades/{id}", controllers.EditaPersonalidade).Methods("Put")
 
 	log.Fatal(http.ListenAndServe(":8000", r))
 }
